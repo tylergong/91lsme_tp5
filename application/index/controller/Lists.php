bf6fe8bc28ff749916fa0378c3b43339af3147da
tylergong <tylergong@163.com>
2017-12-08 15:31:11 +0800

    修改前端 list 可分页

--- application/index/controller/Lists.php
+++ application/index/controller/Lists.php
@@ -28,7 +28,19 @@ class Lists extends Common {
                 ->where('a.is_del', 0)
                 ->order('create_time desc')
                 ->field('a.id,a.title,a.create_time,a.author,a.content,a.thumb,a.click_num,a.cid,c.cname')
-                ->select();
+                ->paginate(6)->each(function ($v, $k) {
+                    // 替换掉各种标签 空格 换行符等
+                    $tmp = preg_replace(array('/<img(.*?)>/', '/<(.*?)>/', '/<\/(.*?)>/', '/<br \/>/', '/&nbsp;/', '/&lt;(.*?)&gt;/'), '', $v['content']);
+                    // 然后在从内容中获取100字摘要
+                    $v['digest'] = mb_substr($tmp, 0, 100, 'utf-8');
+                    // 查询文章标签
+                    $v['tags'] = db('ls_article_tag')->alias('at')
+                        ->join('ls_tag t', 'at.tag_id=t.id')
+                        ->where('at.article_id', $v['id'])
+                        ->field('at.tag_id,t.tag_name')
+                        ->select();
+                    return $v;
+                });
         }
 
         $tag_id = input('param.tag_id');
@@ -49,22 +61,20 @@ class Lists extends Common {
                 ->where('a.is_del', 0)
                 ->order('create_time desc')
                 ->field('a.id,a.title,a.create_time,a.author,a.content,a.thumb,a.click_num,a.cid,c.cname')
-                ->select();
+                ->paginate(6)->each(function ($v, $k) {
+                    // 替换掉各种标签 空格 换行符等
+                    $tmp = preg_replace(array('/<img(.*?)>/', '/<(.*?)>/', '/<\/(.*?)>/', '/<br \/>/', '/&nbsp;/', '/&lt;(.*?)&gt;/'), '', $v['content']);
+                    // 然后在从内容中获取100字摘要
+                    $v['digest'] = mb_substr($tmp, 0, 100, 'utf-8');
+                    // 查询文章标签
+                    $v['tags'] = db('ls_article_tag')->alias('at')
+                        ->join('ls_tag t', 'at.tag_id=t.id')
+                        ->where('at.article_id', $v['id'])
+                        ->field('at.tag_id,t.tag_name')
+                        ->select();
+                    return $v;
+                });
         }
-
-        foreach ($articleData as $k => $v) {
-            // 替换掉各种标签 空格 换行符等
-            $tmp = preg_replace(array('/<img(.*?)>/', '/<(.*?)>/', '/<\/(.*?)>/', '/<br \/>/', '/&nbsp;/', '/&lt;(.*?)&gt;/'), '', $v['content']);
-            // 然后在从内容中获取100字摘要
-            $articleData[$k]['digest'] = mb_substr($tmp, 0, 100, 'utf-8');
-            // 查询文章标签
-            $articleData[$k]['tags'] = db('ls_article_tag')->alias('at')
-                ->join('ls_tag t', 'at.tag_id=t.id')
-                ->where('at.article_id', $v['id'])
-                ->field('at.tag_id,t.tag_name')
-                ->select();
-        }
-
         $this->assign('headData', $headData);
         $this->assign('articleData', $articleData);
         return $this->fetch();
